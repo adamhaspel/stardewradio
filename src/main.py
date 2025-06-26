7a59aaf9900f38f5eb08de076ebede3e60b31bcd
adamhaspel <adam.haspel@gmail.com>
2025-06-25 20:43:31 -0400

    volume control

--- src/main.py
+++ src/main.py
@@ -31,17 +31,16 @@ os.system('clear')
 intents = nextcord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
-bot = commands.Bot(command_prefix="r!", intents=intents,activity = nextcord.Activity(name="r!help", type=2))
+bot = commands.Bot(command_prefix="r!", intents=intents)
 global song
 global songtime
 
 @bot.event
-async def on_connect():
+async def on_connvect():
     print(cs(f'[{time.ctime()}] Info: Logging in as {bot.user.name}. Please stand by...', "green"))
-    await asyncio.sleep(20)
     while True:
-        if os.path.exists("out"):
-            shutil.rmtree("out")
+        if os.path.exists("env/out"):
+            shutil.rmtree("env/out")
             print(cs(f'[{time.ctime()}] Info: Out folder cleared as per routine.', "green"))
         await asyncio.sleep(600)
 
@@ -63,7 +62,9 @@ async def on_ready():
             length = audio.info.length
         for i in bot.guilds:
             if bot.get_guild(i.id).voice_client:
-                source = await nextcord.FFmpegOpusAudio.from_probe(f"assets/audio/ost/{song}", method="fallback")
+                source = nextcord.FFmpegPCMAudio(f"assets/audio/ost/{song}")
+                source = nextcord.PCMVolumeTransformer(source)
+                source.volume = bot.get_guild(i.id).voice_client.volume
                 while True:
                     try:
                         bot.get_guild(i.id).voice_client.play(source)
@@ -75,16 +76,23 @@ async def on_ready():
             xtra = "0"
         print(cs(f'[{time.ctime()}] Song: Now playing "{songname}" for 0{round(length/60)}:{xtra}{round(length % 60)}.', "green"))
         songtime = 0
+        await bot.change_presence(activity=nextcord.Game(name=f"{songname}. Use r!help for more info."))
         while songtime <= length:
             await asyncio.sleep(1)
             songtime += 1
 
 @bot.event
 async def on_disconnect():
+    for i in bot.guilds:
+        if bot.get_guild(i.id).voice_client:
+            await bot.get_guild(i.id).voice_client.disconnect()
     print(cs(f'[{time.ctime()}] Info: Disconnecting from {bot.user.name}. Please stand by...', "green"))
 
 @bot.event
 async def on_close():
+    for i in bot.guilds:
+        if bot.get_guild(i.id).voice_client:
+            await bot.get_guild(i.id).voice_client.disconnect()
     print(cs(f'\r[{time.ctime()}] Info: {bot.user.name} is logging off.', "green"))
 
 @bot.command(help="Connects the bot to a voice channel", aliases=["connect"])
@@ -113,10 +121,116 @@ async def join(ctx):
     audio = AudioSegment.from_mp3(input_file)
     sliced_audio = audio[start_time:end_time]
     sliced_audio.export(output_file, format="mp3")
-    source = await nextcord.FFmpegOpusAudio.from_probe(f"assets/audio/sfx/syntheffect1.mp3", method="fallback")
+    source = nextcord.FFmpegPCMAudio(f"assets/audio/sfx/syntheffect1.mp3")
+    source = nextcord.PCMVolumeTransformer(source)
     ctx.voice_client.play(source)
     await asyncio.sleep(9)
-    source = await nextcord.FFmpegOpusAudio.from_probe(f"env/out/{ctx.guild.id}.mp3", method="fallback")
+    source = nextcord.FFmpegPCMAudio(f"env/out/{ctx.guild.id}.mp3")
+    source = nextcord.PCMVolumeTransformer(source)
+    source.volume = ctx.voice_client.volume
+    ctx.voice_client.play(source)
+
+@bot.command(help="Changes the volume of the radio")
+async def volume(ctx, volume=None):
+    global song
+    global songtime
+    if volume:
+        if not ctx.voice_client:
+            await ctx.send("```I am not in a voice channel. I must be in a voice channel to use this command.```")
+            return
+        else:
+            if ctx.author.voice:
+                if (not ctx.author.voice.channel == ctx.voice_client.channel) or not ctx.author.guild_permissions.move_members:
+                    await ctx.send("```We are not in the same voice channel. We must be in the same voice channel to use this command.```")
+                    return
+                else:
+                    pass
+            else:
+                if ctx.author.guild_permissions.move_member:
+                    pass
+                else:
+                    await ctx.send("```You are not in a voice channel. You must be in a voice channel to use this command.```")
+                    return
+        vc = nextcord.utils.get(bot.voice_clients, guild=ctx.guild)
+        try:
+            int(volume)
+        except:
+            await ctx.send("```Please enter a volume between 0 and 100.```")
+        if 0 <= int(volume) <= 100:
+            vc.source.volume = int(volume)/100
+            vc.volume = int(volume)/100
+            await ctx.send(f"```Volume set to {volume}%.```")
+        else:
+            await ctx.send("```Please enter a volume between 0 and 100.```")
+    else:
+        vc = nextcord.utils.get(bot.voice_clients, guild=ctx.guild)
+        if vc:
+            await ctx.send(f"```Volume is set to {vc.volume*100}%.```")
+        else:
+            await ctx.send("```I am not in a voice channel. I must be in a voice channel to use this command.```")
+
+
+@bot.command(help="Pauses radio playback", aliases=["pause"])
+async def tuneout(ctx):
+    global song
+    global songtime
+    if not ctx.voice_client:
+        await ctx.send("```I am not in a voice channel. I must be in a voice channel to use this command.```")
+        return
+    else:
+        if ctx.author.voice:
+            if (not ctx.author.voice.channel == ctx.voice_client.channel) or not ctx.author.guild_permissions.move_members:
+                await ctx.send("```We are not in the same voice channel. We must be in the same voice channel to use this command.```")
+                return
+            else:
+                await ctx.send(f"```Tuning out...```")
+        else:
+            if ctx.author.guild_permissions.move_member:
+                await ctx.send(f"```Tuning out...```")
+            else:
+                await ctx.send("```You are not in a voice channel. You must be in a voice channel to use this command.```")
+                return
+    if not ctx.voice_client.is_playing():
+        await ctx.send("```I am already paused. The radio must be tuned in for this command to work.```")
+    ctx.voice_client.stop()
+
+@bot.command(help="Continues radio playback after pausing", aliases=["resume"])
+async def tunein(ctx):
+    global song
+    global songtime
+    if not ctx.voice_client:
+        await ctx.send("```I am not in a voice channel. I must be in a voice channel to use this command.```")
+        return
+    else:
+        if ctx.author.voice:
+            if (not ctx.author.voice.channel == ctx.voice_client.channel) or not ctx.author.guild_permissions.move_members:
+                await ctx.send("```We are not in the same voice channel. We must be in the same voice channel to use this command.```")
+                return
+            else:
+                await ctx.send(f"```Tuning back in...```")
+        else:
+            if ctx.author.guild_permissions.move_member:
+                await ctx.send(f"```Tuning back in...```")
+            else:
+                await ctx.send("```You are not in a voice channel. You must be in a voice channel to use this command.```")
+                return
+    if ctx.voice_client.is_playing():
+        await ctx.send("```I am still playing. The radio must be tuned out for this command to work.```")
+    input_file = f"assets/audio/ost/{song}"
+    output_file = f"env/out/{ctx.guild.id}.mp3"
+    start_time = round((9 + songtime)* 1000)
+    audio = File(f"assets/audio/ost/{song}")
+    end_time = round(1000* audio.info.length)
+    audio = AudioSegment.from_mp3(input_file)
+    sliced_audio = audio[start_time:end_time]
+    sliced_audio.export(output_file, format="mp3")
+    source = nextcord.FFmpegPCMAudio(f"assets/audio/sfx/syntheffect1.mp3")
+    source = nextcord.PCMVolumeTransformer(source)
+    ctx.voice_client.play(source)
+    await asyncio.sleep(9)
+    source = nextcord.FFmpegPCMAudio(f"env/out/{ctx.guild.id}.mp3")
+    source = nextcord.PCMVolumeTransformer(source)
+    source.volume = ctx.voice_client.volume
     ctx.voice_client.play(source)
 
 @bot.command(help="Disonnects the bot from a voice channel", aliases=["disconnect"])
